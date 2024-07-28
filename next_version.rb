8ce9ff976090bea311248507e5409dbadf0d5615
Caio Bianchi <CaioBianchi@users.noreply.github.com>
2024-07-27 21:48:11 -0400

    Lint

--- next_version.rb
+++ next_version.rb
@@ -1,27 +1,25 @@
-=begin
-Semantic Versions
-
-Description:
-You're fed up about changing the version of your software manually. Instead, you will create a little script that will make it for you.
-
-Challenge:
-Create a function nextVersion, that will take a string in parameter, and will return a string containing the next version number.
-
-For example:
-
-```
-nextVersion("1.2.3") === "1.2.4";
-nextVersion("0.9.9") === "1.0.0.";
-nextVersion("1") === "2";
-nextVersion("1.2.3.4.5.6.7.8") === "1.2.3.4.5.6.7.9";
-nextVersion("9.9") === "10.0";
-```
-
-Rules
-
-No number but the first may be greater than 9: if any are, you must set them to 0 and increment the next number in sequence.
-You can assume all tests inputs to be valid.
-=end
+# Semantic Versions
+#
+# Description:
+# You're fed up about changing the version of your software manually. Instead, you will create a little script that will make it for you.
+#
+# Challenge:
+# Create a function nextVersion, that will take a string in parameter, and will return a string containing the next version number.
+#
+# For example:
+#
+# ```
+# nextVersion("1.2.3") === "1.2.4";
+# nextVersion("0.9.9") === "1.0.0.";
+# nextVersion("1") === "2";
+# nextVersion("1.2.3.4.5.6.7.8") === "1.2.3.4.5.6.7.9";
+# nextVersion("9.9") === "10.0";
+# ```
+#
+# Rules
+#
+# No number but the first may be greater than 9: if any are, you must set them to 0 and increment the next number in sequence.
+# You can assume all tests inputs to be valid.
 
 def nextVersion(version)
   # Split the version string into an array of integers
@@ -38,7 +36,6 @@ def nextVersion(version)
       parts[i] = 0
     end
   end
-
   # If all parts were 9, prepend a 1 at the beginning
   parts.unshift(1)
   parts.join('.')
